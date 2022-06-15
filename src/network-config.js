fd3926b495f2b6da9b78e201cc14d08945804666
Sem <931684+sembrestels@users.noreply.github.com>
2022-06-15 15:02:48 +0200

    Change gnosis chain rpc

--- src/network-config.js
+++ src/network-config.js
@@ -122,7 +122,7 @@ export const networkConfigs = {
         localEnsRegistryAddress || '0xaafca6b0c89521752e559650206d7c925fd0e530',
     },
     nodes: {
-      defaultEth: 'wss://xdai.poanetwork.dev/wss',
+      defaultEth: 'wss://rpc.gnosischain.com/wss',
     },
     settings: {
       chainId: 100,
